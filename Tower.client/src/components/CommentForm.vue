85425801b179fcb56127d502b535cd978daaeef4
TobyComon <toby.comon@gmail.com>
2022-05-10 16:16:27 -0600

    small changes

--- Tower.client/src/components/CommentForm.vue
+++ Tower.client/src/components/CommentForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+  <div class="container">
     <form @submit.prevent="create" id="commentForm" class="text-center">
       <div class="mb-3">
         <label for="body" class="form-label"></label>
@@ -15,8 +15,8 @@
         />
       </div>
       <button class="btn btn-success" type="submit">Submit</button>
-</form>
-</div>
+    </form>
+  </div>
 </template>
 
 
@@ -26,29 +26,28 @@ import { useRoute } from 'vue-router'
 import Pop from '../utils/Pop.js'
 import { logger } from '../utils/Logger.js'
 import { commentsService } from '../services/CommentsService.js'
-import { Modal } from 'bootstrap'
+
 export default {
-    setup(){
-        const route = useRoute()
-        const editable = ref({eventId: route.params.id})
-        return {
-            editable,
-            async create() {
-                try {
-                  await commentsService.createComment(editable.value)
-                  document.getElementById("commentForm").reset();
-                  
-                } catch (error) {
-                  logger.error(error)
-                  Pop.toast(error.message, 'error')
-                }
-            }
+  setup() {
+    const route = useRoute()
+    const editable = ref({ eventId: route.params.id })
+    return {
+      editable,
+      async create() {
+        try {
+          await commentsService.createComment(editable.value)
+          document.getElementById("commentForm").reset();
+
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message, 'error')
         }
+      }
     }
+  }
 }
 </script>
 
 
 <style lang="scss" scoped>
-
 </style>
--- Tower.client/src/components/MyTicket.vue
+++ Tower.client/src/components/MyTicket.vue
@@ -1,15 +1,18 @@
 <template>
-    
-        
-            <div class="text-light">
-                <div>
-                    <h3>{{ticket.event.name}}</h3>
-                    <i class="mdi mdi-trash-can-outline text-light selectable" @click="cancelTicket"></i>
-                </div>
-                <img class="img-fluid" :src="ticket.event.coverImg" :alt="ticket.event.coverImg">
-            </div>
-        
-    
+  <div class="text-light">
+    <div>
+      <h3>{{ ticket.event.name }}</h3>
+      <i
+        class="mdi mdi-trash-can-outline text-light selectable"
+        @click="cancelTicket"
+      ></i>
+    </div>
+    <img
+      class="img-fluid"
+      :src="ticket.event.coverImg"
+      :alt="ticket.event.coverImg"
+    />
+  </div>
 </template>
 
 
@@ -20,32 +23,31 @@ import { ticketsService } from '../services/TicketsService.js'
 import Pop from '../utils/Pop.js'
 import { logger } from '../utils/Logger.js'
 export default {
-    props: {
+  props: {
     ticket: {
       type: Object,
       required: true
     }
   },
-    setup(props){
-        return {
-            myTickets: computed(() => AppState.myTickets),
-            async cancelTicket() {
-                try {
-                  if(await Pop.confirm('Cancel Ticket?')){
-                      await ticketsService.cancelTicket(props.ticket.id)
-                    Pop.toast('Ticket Cancelled', 'success')
-                  }
-                } catch (error) {
-                  logger.error(error)
-                  Pop.toast(error.message, 'error')
-                }
-            }
+  setup(props) {
+    return {
+      myTickets: computed(() => AppState.myTickets),
+      async cancelTicket() {
+        try {
+          if (await Pop.confirm('Cancel Ticket?')) {
+            await ticketsService.cancelTicket(props.ticket.id)
+            Pop.toast('Ticket Cancelled', 'success')
+          }
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message, 'error')
         }
+      }
     }
+  }
 }
 </script>
 
 
 <style lang="scss" scoped>
-
 </style>
